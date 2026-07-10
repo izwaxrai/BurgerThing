80033bfaa4348607dab755b86181dd727af59e97
izwaxrai <raixhee@gmail.com>
2026-07-10 12:16:27 +0800

    Update app title and configuration settings

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,4 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+# ---- CONFIGURATION & STYLING ----
+st.set_page_config(page_title="BurgerThing E-Kiosk", page_icon="🍔", layout="wide")
